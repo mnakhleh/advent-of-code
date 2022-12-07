235ed32a7bbd83146c9fdc08f83f76acb2dad32c
Marc Nakhleh <mnakhleh@trakmaps.com>
2022-12-07 02:21:11 -0500

    Cleaning

--- 2022/day07.py
+++ 2022/day07.py
@@ -1,7 +1,6 @@
-from data.day07 import SAMPLE_TXT, INPUT_TXT
 from anytree import Node, LevelOrderIter
-from typing import Iterator
 
+from data.day07 import SAMPLE_TXT, INPUT_TXT
 
 TOTAL_SPACE = 70_000_000
 NEEDED_SPACE = 30_000_000
@@ -9,38 +8,28 @@ NEEDED_SPACE = 30_000_000
 
 def part1(txt: str) -> int:
     root = get_file_tree(txt)
-    return sum(len(dr) for dr in get_small_dirs(root, 100000))
+    return sum(len(node) for node in LevelOrderIter(root) if len(node) <= 100000)
 
 
 def part2(txt: str) -> int:
     root = get_file_tree(txt)
     space_to_free = abs(TOTAL_SPACE - NEEDED_SPACE - len(root))
 
-    all_dirs = [len(node) for node in LevelOrderIter(root) if node.typ == 'dir']
-    for size in sorted(all_dirs):
-        if size >= space_to_free:
-            return size
+    dir_sizes = [len(node) for node in LevelOrderIter(root)]
+
+    # Taken from bucketz76. Love it
+    return min(size for size in dir_sizes if size >= space_to_free)
 
 
 class SizeNode(Node):
     size: int
-    typ: str
 
     def __len__(self) -> int:
-        if self.typ == 'file':
-            return self.size
-
-        total_size = 0
-        for child in self.children:
-            if child.typ == 'file':
-                total_size += child.size
-            else:
-                total_size += len(child)
-        return total_size
+        return self.size + sum(len(child) for child in self.children)
 
 
 def get_file_tree(txt: str) -> SizeNode:
-    root = SizeNode('root', typ='dir')
+    root = SizeNode('root', size=0)
     cur_node = root
     commands = txt.splitlines()[1:]
     for i, command in enumerate(commands):
@@ -49,29 +38,16 @@ def get_file_tree(txt: str) -> SizeNode:
         elif command == '$ cd /':
             cur_node = root
         elif command.startswith('$ cd'):
-            new_node = SizeNode(command[5:], typ='dir', parent=cur_node)
+            new_node = SizeNode(command[5:], size=0, parent=cur_node)
             cur_node = new_node
         elif command == '$ ls':
-            j = 0
-            while True:
-                j += 1
-                if len(commands) == i + j:  # Reached end of the list of commands
-                    break
-                potential_file = commands[i + j]
-                if potential_file.startswith('$'):
-                    break
-                if potential_file.startswith('dir'):
-                    continue
-                else:
-                    size, name = potential_file.split(' ')
-                    SizeNode(name, typ='file', size=int(size), parent=cur_node)
+            continue
+        elif command.startswith('dir'):
+            continue
+        else:  # File size
+            size = command.split(' ')[0]
+            cur_node.size += int(size)
     return root
-
-
-def get_small_dirs(node: SizeNode, threshhold: int) -> Iterator[SizeNode]:
-    for node in LevelOrderIter(node):
-        if node.typ == 'dir' and len(node) <= threshhold:
-            yield node
 
 
 ##########################
@@ -80,4 +56,3 @@ if __name__ == '__main__':
     print(part1(INPUT_TXT))
     assert part2(SAMPLE_TXT) == 24933642
     print(part2(INPUT_TXT))
-
