23e4cebe2aff40e0ab97965a5b2f81553f8974a8
Marc Nakhleh <mnakhleh@trakmaps.com>
2022-12-07 02:25:22 -0500

    Trying to be less clever

--- 2022/day07.py
+++ 2022/day07.py
@@ -8,28 +8,25 @@ NEEDED_SPACE = 30_000_000
 
 def part1(txt: str) -> int:
     root = get_file_tree(txt)
-    return sum(len(node) for node in LevelOrderIter(root) if len(node) <= 100000)
+    return sum(size(node) for node in LevelOrderIter(root) if size(node) <= 100000)
 
 
 def part2(txt: str) -> int:
     root = get_file_tree(txt)
-    space_to_free = abs(TOTAL_SPACE - NEEDED_SPACE - len(root))
+    space_to_free = abs(TOTAL_SPACE - NEEDED_SPACE - size(root))
 
-    dir_sizes = [len(node) for node in LevelOrderIter(root)]
+    dir_sizes = [size(node) for node in LevelOrderIter(root)]
 
     # Taken from bucketz76. Love it
     return min(size for size in dir_sizes if size >= space_to_free)
 
 
-class SizeNode(Node):
-    size: int
-
-    def __len__(self) -> int:
-        return self.size + sum(len(child) for child in self.children)
+def size(node: Node) -> int:
+    return node.size + sum(size(child) for child in node.children)
 
 
-def get_file_tree(txt: str) -> SizeNode:
-    root = SizeNode('root', size=0)
+def get_file_tree(txt: str) -> Node:
+    root = Node('root', size=0)
     cur_node = root
     commands = txt.splitlines()[1:]
     for i, command in enumerate(commands):
@@ -38,15 +35,15 @@ def get_file_tree(txt: str) -> SizeNode:
         elif command == '$ cd /':
             cur_node = root
         elif command.startswith('$ cd'):
-            new_node = SizeNode(command[5:], size=0, parent=cur_node)
+            new_node = Node(command[5:], size=0, parent=cur_node)
             cur_node = new_node
         elif command == '$ ls':
             continue
         elif command.startswith('dir'):
             continue
         else:  # File size
-            size = command.split(' ')[0]
-            cur_node.size += int(size)
+            file_size = int(command.split(' ')[0])
+            cur_node.size += file_size
     return root
 
 
